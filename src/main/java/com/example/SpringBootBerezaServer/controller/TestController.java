aa985e7f6aa2ca17d16969a5a95acb0eac6a7e4d
Nikolay Punko <punkon2001@gmail.com>
2024-04-24 12:33:28 +0300

    .

--- src/main/java/com/example/SpringBootBerezaServer/controller/TestController.java
+++ src/main/java/com/example/SpringBootBerezaServer/controller/TestController.java
@@ -7,12 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.stream.XMLEventReader;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.StartElement;
-import javax.xml.stream.events.XMLEvent;
-import java.io.StringReader;
 
 @RestController
 @RequestMapping("/test")
