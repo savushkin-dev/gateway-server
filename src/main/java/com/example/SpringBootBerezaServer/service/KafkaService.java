a7c291b5f23aacd548355b2ac962e5338c9bb03d
Nikolay Punko <punkon2001@gmail.com>
2024-04-29 16:37:45 +0300

    .

---
+++ src/main/java/com/example/SpringBootBerezaServer/service/KafkaService.java
@@ -0,0 +1,27 @@
+package com.example.SpringBootBerezaServer.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+
+    @Autowired
+    public KafkaService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+
+    public void sendMessage(String message, String topicName) {
+        log.info("Sending : {} to {} topic", message, topicName);
+        log.info("--------------------------------");
+
+        kafkaTemplate.send(topicName, message);
+    }
+}
