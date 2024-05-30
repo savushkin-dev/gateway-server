3bf1e1a8bd812ce3e471d2023358091ec6d91dbc
Nikolay Punko <punkon2001@gmail.com>
2024-05-30 16:14:15 +0300

    Добавлены тесты для обращения по адресу nastohost и authenticate

--- src/main/java/com/host/SpringBootBerezaServer/dto/LoginRequestDTO.java
+++ src/main/java/com/host/SpringBootBerezaServer/dto/LoginRequestDTO.java
@@ -1,10 +1,14 @@
 package com.host.SpringBootBerezaServer.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequestDTO {
 
     private String username;
