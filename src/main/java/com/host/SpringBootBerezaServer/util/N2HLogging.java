c283d3ebeb0c4a5dfb0087d24264b527add75284
NikolayPunko <108266752+NikolayPunko@users.noreply.github.com>
2024-06-28 12:28:44 +0300

    Изменено логирование, парсинг (#7)
    * Дублирующую логику вынес в отдельный класс; Унаследовал Host2Nas и Nas2Host от одного класса; Добавил проверку на пустоту MessageData XML;
    
    * Переименовал com.example в com.host
    
    * Перенес логирование запросов nas2host и host2nas в логи приложения. Изменил валидацию даты и времени на новый формат.
    
    * Отредактирован парсинг наличия тела сообщения
    
    * Прокомментированы закомментированные строки кода

---
+++ src/main/java/com/host/SpringBootBerezaServer/util/N2HLogging.java
@@ -0,0 +1,27 @@
+package com.host.SpringBootBerezaServer.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class N2HLogging {
+
+    public static void writeLogN2H(String request, long durParsing, long durDB, long durKafka) {
+        log.info("\nDuration parsing+validation: " + durParsing + "\n"
+                + "Duration save in DB: " + durDB + "\n"
+                + "Duration send to kafka: " + durKafka + "\n\n"
+                + "Request:" + "\n"
+                + request + "\n");
+
+    }
+
+    public static void writeLogN2H(String request, String error, long durParsing, long durDB, long durKafka) {
+        log.info("\nDuration parsing+validation: " + durParsing + "\n"
+                + "Duration save in DB: " + durDB + "\n"
+                + "Duration send to kafka: " + durKafka + "\n\n"
+                + "Request:" + "\n"
+                + request + "\n\n"
+                + "Error:" + "\n"
+                + error + "\n");
+
+    }
+}
