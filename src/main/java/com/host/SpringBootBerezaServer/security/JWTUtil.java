53485f6fdceaddef6b110d5cc1d8240c9f89366e
punkon2001@gmail.com <punkon2001@gmail.com>
2025-04-25 08:09:01 +0300

    Изменено время действия токена авторизации на 50 лет

--- src/main/java/com/host/SpringBootBerezaServer/security/JWTUtil.java
+++ src/main/java/com/host/SpringBootBerezaServer/security/JWTUtil.java
@@ -20,7 +20,7 @@ public class JWTUtil {
     private String secret; //The secret key
 
     public String generateToken(String username) {
-        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(60*24*365).toInstant());
+        Date expirationDate = Date.from(ZonedDateTime.now().plusMinutes(60*24*365*50).toInstant());
 
         return JWT.create()
                 .withSubject("User details")
