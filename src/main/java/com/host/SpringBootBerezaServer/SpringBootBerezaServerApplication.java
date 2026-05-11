240dfc9f91444adb59640444a85718697d79d0e7
NikolayPunko <punkon2001@gmail.com>
2026-05-11 09:33:58 +0300

    Убрано взаимодействие с кафкой

--- src/main/java/com/host/SpringBootBerezaServer/SpringBootBerezaServerApplication.java
+++ src/main/java/com/host/SpringBootBerezaServer/SpringBootBerezaServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 
-@EnableKafka
 @SpringBootApplication
 public class SpringBootBerezaServerApplication {
 
