de6046a774d9b7f19bf03a45ee20fe42ed7c6fbd
NikolayPunko <punkon2001@gmail.com>
2026-05-11 10:19:49 +0300

    Внедрен .env файл для конфигурации приложения

--- src/main/java/com/host/SpringBootBerezaServer/SpringBootBerezaServerApplication.java
+++ src/main/java/com/host/SpringBootBerezaServer/SpringBootBerezaServerApplication.java
@@ -3,7 +3,6 @@ package com.host.SpringBootBerezaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.EnableKafka;
 
 
 @SpringBootApplication
